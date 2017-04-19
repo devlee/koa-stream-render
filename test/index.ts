904bd58afd9df3afbb3794fe7ddd037116613683
dianping <dianping@dianpingdeMacBook-Pro.local>
2017-04-19 14:05:57 +0800

    test

--- test/index.ts
+++ test/index.ts
@@ -1,21 +1,21 @@
-// import * as Koa from 'koa';
+import * as Koa from 'koa';
 import * as Chai from 'chai';
 import * as request from 'supertest';
 
 import KoaStreamRender from '../dist';
 import RenderContex from '../dist/interface';
 
-// const app = new Koa();
-// app.use(KoaStreamRender());
-// app.use((ctx: RenderContex) => { ctx.render('test'); })
+const app = new Koa();
+app.use(KoaStreamRender());
+app.use((ctx: RenderContex) => { ctx.render('test'); })
 
 let server;
 describe('render test', () => {
   before(() => {
-    // server = app.listen(3000)
+    server = app.listen(3000)
   })
   after(() => {
-    // server.close();
+    server.close();
   })
   it('should succeed', function (done) {
     request(server)
